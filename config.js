0d0638e476661e8629e041f7758c626c16f22557
Kingrasinda <kingrasinda@gmail.com>
2024-10-22 23:28:20 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=bygB1ThC#YRawrePiaTCBZHaEXW7zlW_JRkr2UaW5ZE50FH_Llhw",
-MONGODB: process.env.MONGODB || "mongodb+srv://supnu:shan27@cluster0.dw38v.mongodb.net/",//enter mongo db url
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=nvxyHb4Q#j-_zlAw75X2eNEp2beeuNSb5CQQJG21NZte1WCioGj0",
+MONGODB: process.env.MONGODB || "mongodb+srv://ponnapkaya1:CUfG1C4P0RUfFsSk@cluster0.7qsf0.mongodb.net/",//enter mongo db url
 };
